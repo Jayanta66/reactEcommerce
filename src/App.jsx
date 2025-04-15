bae2025fa505f770e1ae204df7042afa51a9985d
Jayanta66 <jayhalder18@gmail.com>
2025-04-15 21:34:12 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Checkout from './components/Checkout.jsx';
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
 import Donate from './components/Donate.jsx';
+import SpringBoot from './components/SpringBoot.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
 import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
 
@@ -12,6 +13,7 @@ function App() {
     <UserProgressContextProvider>
       <CartContextProvider>
         <Header />
+        <SpringBoot/>
         <Donate/>
         <Meals />
         <Cart />
---
+++ src/components/SpringBoot.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function SpringBoot() {
+    return <div><h3>You may post anything here using <span>"Post"</span> button.</h3>
+      
+        <a href="https://myliveweb.onrender.com/" target="_blank" rel="noopener noreferrer">
+    Post
+  </a>
+
+
+    </div> 
+
+  }
+  
+  export default SpringBoot;
