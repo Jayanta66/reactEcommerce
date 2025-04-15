34b2cb3f7b2796718028f328be968e6b0c915e8f
Jayanta66 <jayhalder18@gmail.com>
2025-04-15 22:14:17 +0530

    add

--- src/components/SpringBoot.jsx
+++ src/components/SpringBoot.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 
 function SpringBoot() {
-    return <div><h3>You may post anything here using <span>"Post"</span> button.</h3>
+    return <div><h3>You may post anything here using <a href="https://myliveweb.onrender.com/" target="_blank" rel="noopener noreferrer">
+    Post
+  </a> button .</h3>
       
         <a href="https://myliveweb.onrender.com/" target="_blank" rel="noopener noreferrer">
     Post
