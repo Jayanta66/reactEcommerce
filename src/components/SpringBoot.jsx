85700c76c5cd6c3a350cc9ce258d7620ebf442f6
Jayanta66 <jayhalder18@gmail.com>
2025-04-16 19:07:47 +0530

    add

--- src/components/SpringBoot.jsx
+++ src/components/SpringBoot.jsx
@@ -6,17 +6,17 @@ function SpringBoot() {
     return <div><h1>We provide tech solution like :</h1>
       <h2>* website build (ওয়েবসাইট তৈরি).</h2>
       <h2>* website develop(ওয়েবসাইট তৈরি করা) .</h2>
+      <h2>* website update or upgrade (ওয়েবসাইট তৈরি করা) .</h2>
       <h2>* website customise (ওয়েবসাইট কাস্টমাইজ করুন).</h2>
       <h2>* fix any kind of issue of website (ওয়েবসাইটের যেকোনো ধরণের সমস্যা সমাধান করুন) .</h2>
       <h2>* digitalize any kind of idea or business (যেকোনো ধরণের ধারণা বা ব্যবসা ডিজিটালাইজ করুন).</h2>
-
-      <h3>You may post anything here using  <a href="https://myliveweb.onrender.com/" target="_blank" rel="noopener noreferrer">
+      <h3>You may post anything here using  <a href="https://tiptops.xyz/" target="_blank" rel="noopener noreferrer">
     Post
-  </a> button <span>( আপনি এখানে যেকোনো কিছু পোস্ট করতে পারেন </span><a href="https://myliveweb.onrender.com/" target="_blank" rel="noopener noreferrer">
+  </a> button <span>( আপনি এখানে যেকোনো কিছু পোস্ট করতে পারেন </span><a href="https://tiptops.xyz/" target="_blank" rel="noopener noreferrer">
     Post
   </a>  <span> বোতাম ব্যবহার করে )</span> .</h3>
       
-        <a href="https://myliveweb.onrender.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://tiptops.xyz/" target="_blank" rel="noopener noreferrer">
     Post
   </a>
 
